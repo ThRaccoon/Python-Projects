0dc53bcd5fb9737caa406ebdabb7a74d300df1a4
ThRaccoon <136110918+ThRaccoon@users.noreply.github.com>
2024-02-06 18:22:09 +0200

    Add files via upload

---
+++ Python Projects/encryptAndDecrypt/decrypt.py
@@ -0,0 +1,16 @@
+import alphabet
+
+
+def decrypt(text, key):
+    text = list(text)
+    key = key % 26
+    position = 0
+    for r in range(0, len(text)):
+        if text[r].isspace() or text[r].isdigit():
+            text[r] = text[r]
+        else:
+            position = alphabet.alphabetENG.index(text[r])
+            position -= key
+            text[r] = alphabet.alphabetENG[position]
+
+    return ''.join(text)
